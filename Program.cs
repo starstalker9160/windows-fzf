a566bdab389752e2a8542095eb54a27bdec5885e
starstalker9160 <abyazjschool@gmail.com>
2024-09-13 08:46:18 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        if (args.Length == 0) {
+            Console.WriteLine("Useage: fzf [searchTerm]");
+            
+        }
+    }
+}
