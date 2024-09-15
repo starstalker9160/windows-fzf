b864c6c678c63497467ebbcb02f71837fbd1f0db
starstalker9160 <abyazjschool@gmail.com>
2024-09-15 13:10:53 +0400

    made searching better, faster and stronger

--- Program.cs
+++ Program.cs
@@ -33,10 +33,52 @@ class Program {
         return;
     }
 
+    private static string getDbPath() {
+        string dbFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "windows-fzf");
+        Directory.CreateDirectory(dbFolder);
+        return Path.Combine(dbFolder, "database.db");
+    }
+
+    private static void helpMenu() {
+        Console.WriteLine(@"
+Usage: fzf [command] [options]
+
+Commands:
+  -s [searchTerm]    Search for files in the database.
+  -createdb          Create a new database with files to search within the db.
+  -updatedb          Update the database with paths of all files in the current directory and subdirectories.
+  -cleardb           Clear the database or delete the folder based on the provided options.
+
+Options for -cleardb:
+  -d                 Delete the folder and database (will fail if there are other items in the folder).           
+
+Example Usage:
+  fzf -createdb
+  fzf -updatedb
+  fzf -cleardb -d
+        ");
+    }
+
     private static void printErr(string s) {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine(s);
         Console.ResetColor();
+    }
+
+    private static void createDb() {
+        string dbPath = getDbPath();
+
+        if (!File.Exists(dbPath)) {
+            SQLiteConnection.CreateFile(dbPath);
+            using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;")) {
+                connection.Open();
+                using (var command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Files (Id INTEGER PRIMARY KEY AUTOINCREMENT, Path TEXT)", connection)) { command.ExecuteNonQuery(); }
+            }
+            Console.WriteLine($"Database created succesfully, please use `fzf -updatedb to add enteries to the database`");
+        } else {
+            printErr("Error: Database already exists.");
+            Console.WriteLine("Please use `fzf -cleardb -d` first and then try running this command again");
+        }
     }
 
     private static void search(string[] searchTerms) {
@@ -48,11 +90,11 @@ class Program {
 
         string wd = Directory.GetCurrentDirectory() + "\\";
         List<string> stuffInWd = new List<string>();
+        var segments = new List<List<string>>();
 
         using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;")) {
             connection.Open();
-            string query = "SELECT Path FROM Files WHERE Path LIKE @wdPrefix";
-            using (var command = new SQLiteCommand(query, connection)) {
+            using (var command = new SQLiteCommand("SELECT Path FROM Files WHERE Path LIKE @wdPrefix", connection)) {
                 command.Parameters.AddWithValue("@wdPrefix", $"{wd}%");
                 using (var reader = command.ExecuteReader()) {
                     while (reader.Read()) { stuffInWd.Add(reader["Path"].ToString().Substring(wd.Length)); }
@@ -60,7 +102,6 @@ class Program {
             }
         }
 
-        var segments = new List<List<string>>();
         for (int i = 0; i < stuffInWd.Count; i += 50) {
             segments.Add(stuffInWd.Skip(i).Take(50).ToList());
         }
@@ -93,57 +134,9 @@ class Program {
 
         foreach (var thread in threads) { thread.Join(); }
     
-        foreach (var result in searchResults) { Console.WriteLine(result); }
-    }
+        foreach (var result in searchResults) { Console.WriteLine( "\\" + result); }
 
-    public static bool onlyFile(string directoryPath) {
-        if (string.IsNullOrEmpty(directoryPath) || string.IsNullOrEmpty("database.db")) return false;
-        var files = Directory.GetFiles(directoryPath);
-        return files.Length == 1 && Path.GetFileName(files[0]) == "database.db";
-    }
-
-    private static string getDbPath() {
-        string dbFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "windows-fzf");
-        Directory.CreateDirectory(dbFolder);
-        return Path.Combine(dbFolder, "database.db");
-    }
-
-    private static void helpMenu() {
-        Console.WriteLine(@"
-Usage: fzf [command] [options]
-
-Commands:
-  -s [searchTerm]    Search for files in the database.
-  -createdb          Create a new database with files to search within the db.
-  -updatedb          Update the database with paths of all files in the current directory and subdirectories.
-  -cleardb           Clear the database or delete the folder based on the provided options.
-
-Options for -cleardb:
-  -d                 Delete the folder and database (will fail if there are other items in the folder).           
-
-Example Usage:
-  fzf -createdb
-  fzf -updatedb
-  fzf -cleardb -d
-        ");
-    }
-
-
-    private static void createDb() {
-        string dbPath = getDbPath();
-
-        if (!File.Exists(dbPath)) {
-            SQLiteConnection.CreateFile(dbPath);
-            using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;")) {
-                connection.Open();
-                string createTableQuery = "CREATE TABLE IF NOT EXISTS Files (Id INTEGER PRIMARY KEY AUTOINCREMENT, Path TEXT)";
-                using (var command = new SQLiteCommand(createTableQuery, connection)) { command.ExecuteNonQuery(); }
-            }
-            Console.WriteLine($"Database created succesfully, please use `fzf -updatedb to add enteries to the database`");
-        } else {
-            printErr("Error: Database already exists.");
-            Console.WriteLine("Please use `fzf -cleardb -d` first and then try running this command again");
-        }
+        searchResults.Clear(); segments.Clear(); stuffInWd.Clear();
     }
 
     private static void updateDb() {
@@ -151,48 +144,97 @@ Example Usage:
 
         if (!File.Exists(dbPath)) {
             printErr("Error: No database found to update.");
-            Console.WriteLine("Please use `fzf -createdb` first and then try running this command again");
+            Console.WriteLine("Please use `fzf -createdb` first and then try running this command again.");
             return;
         }
 
+        string wd = Directory.GetCurrentDirectory() + "\\";
+        List<string> stuffInWd = new List<string>();
+        string[] dirsInWd = Directory.GetDirectories(wd);
+        stuffInWd.AddRange(Directory.GetFiles(wd));
+        stuffInWd.AddRange(dirsInWd);
+
         using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;")) {
             connection.Open();
-
-            var currentFilesInDb = new HashSet<string>();
-            string selectQuery = "SELECT Path FROM Files";
-            using (var command = new SQLiteCommand(selectQuery, connection)) {
-                using (var reader = command.ExecuteReader()) {
-                    while (reader.Read()) {
-                        currentFilesInDb.Add(reader.GetString(0));
-                    }
-                }
+            using (var command = new SQLiteCommand("DELETE FROM Files WHERE Path LIKE @wdPrefix", connection)) {
+                command.Parameters.AddWithValue("@wdPrefix", $"{wd}%");
+                command.ExecuteNonQuery();
             }
-
-            // Iterate over all the files in the current directory and subdirectories
-            foreach (var file in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.*", SearchOption.AllDirectories)) {
-                if (!currentFilesInDb.Contains(file)) {
-                    // If the file is not already in the database, insert it
-                    string insertQuery = "INSERT INTO Files (Path) VALUES (@path)";
-                    using (var insertCommand = new SQLiteCommand(insertQuery, connection)) {
-                        insertCommand.Parameters.AddWithValue("@path", file);
-                        insertCommand.ExecuteNonQuery();
-                    }
-                }
-            }
-
-            // Optionally, remove entries from the database for files that no longer exist
-            var currentFilesOnDisk = new HashSet<string>(Directory.GetFiles(Directory.GetCurrentDirectory(), "*.*", SearchOption.AllDirectories));
-            foreach (var fileInDb in currentFilesInDb) {
-                if (!currentFilesOnDisk.Contains(fileInDb)) {
-                    string deleteQuery = "DELETE FROM Files WHERE Path = @path";
-                    using (var deleteCommand = new SQLiteCommand(deleteQuery, connection)) {
-                        deleteCommand.Parameters.AddWithValue("@path", fileInDb);
-                        deleteCommand.ExecuteNonQuery();
-                    }
-                }
-            }
-            Console.WriteLine("Database updated with file paths.");
         }
+
+        var stuff = new HashSet<string>(stuffInWd);
+
+        void ProcessDirectory(string dir) {
+            try {
+                foreach (var file in Directory.GetFiles(dir)) {
+                    stuff.Add(file);
+                }
+                foreach (var subDir in Directory.GetDirectories(dir)) {
+                    ProcessDirectory(subDir);
+                }
+            } catch (UnauthorizedAccessException) {
+                printErr($"Error : Access denied to directory {dir}, skipping it.");
+            }
+        }
+
+        var dirsQueue = new Queue<string>(dirsInWd);
+        
+        void ThreadProcess() {
+            while (true) {
+                string dir;
+                lock (dirsQueue) {
+                    if (dirsQueue.Count == 0) break;
+                    dir = dirsQueue.Dequeue();
+                }
+                ProcessDirectory(dir);
+            }
+        }
+
+        var threads = new List<Thread>();
+        for (int i = 0; i < 3; i++) {
+            var thread = new Thread(ThreadProcess);
+            threads.Add(thread);
+            thread.Start();
+        }
+
+        foreach (var thread in threads) {
+            thread.Join();
+        }
+
+        void AddToDatabase(HashSet<string> paths) {
+            using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;")) {
+                connection.Open();
+                foreach (var path in paths) {
+                    using (var command = new SQLiteCommand("INSERT OR IGNORE INTO Files (Path) VALUES (@path)", connection)) {
+                        command.Parameters.AddWithValue("@path", path);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        void AddStuffInWdToDatabase() {
+            using (var connection = new SQLiteConnection($"Data Source={dbPath};Version=3;")) {
+                connection.Open();
+                foreach (var path in stuffInWd) {
+                    using (var command = new SQLiteCommand("INSERT OR IGNORE INTO Files (Path) VALUES (@path)", connection)) {
+                        command.Parameters.AddWithValue("@path", path);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        var addToDatabaseThread1 = new Thread(() => AddToDatabase(stuff));
+        var addToDatabaseThread2 = new Thread(AddStuffInWdToDatabase);
+
+        addToDatabaseThread1.Start();
+        addToDatabaseThread2.Start();
+
+        addToDatabaseThread1.Join();
+        addToDatabaseThread2.Join();
+
+        Console.WriteLine("Database update complete.");
     }
 
 
